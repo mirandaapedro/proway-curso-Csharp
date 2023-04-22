e7b0dbebd280712298c79a41d261950d9a656751
PEDRO HENRIQUE MIRANDA <73290@PROWAY.TREINA>
2023-04-22 16:49:17 -0300

    Adicionado exemplo vazio do Laço de Repetição WHILE

---
+++ Fundamentos/LacoRepeticao/LacoRepeticaoWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.LacoRepeticao
+{
+    internal class LacoRepeticaoWhile
+    {
+        // Método sem retorno, dica: void -> se fosse public double teria retorno.
+        // Método sem parâmetros, dica: não tem info/variáveis dentro dos parênteses
+        public void Executar()
+        {
+            Exemplo01();
+        }
+
+        public void Exemplo01()
+        {
+            Console.WriteLine("OIEEEEEEEEE");
+        }
+    }
+}
