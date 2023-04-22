02c9a9fd1913bcb6807f9f440d17c5fb3a26fa99
PEDRO HENRIQUE MIRANDA <73290@PROWAY.TREINA>
2023-04-22 18:03:47 -0300

    Adicionado exemplos de WHILE com Menu

--- Fundamentos/LacoRepeticao/LacoRepeticaoWhile.cs
+++ Fundamentos/LacoRepeticao/LacoRepeticaoWhile.cs
@@ -12,12 +12,120 @@ namespace Fundamentos.LacoRepeticao
         // Método sem parâmetros, dica: não tem info/variáveis dentro dos parênteses
         public void Executar()
         {
-            Exemplo01();
+            int opcaoDesejada = 0;
+
+
+            while(opcaoDesejada != 8001)
+            {
+                Console.WriteLine("\n1 - Exemplo 01" +
+                    "\n2 - Exemplo 02" + 
+                    "\n3 - Exemplo 03" +
+                    "\n8001 - Sair");
+                opcaoDesejada = Convert.ToInt32(Console.ReadLine());    
+                if (opcaoDesejada == 1)
+                {
+                    Exemplo01();
+                }
+                else if (opcaoDesejada == 2)
+                {
+                    Exemplo02();
+                }
+                else if (opcaoDesejada == 3)
+                {
+                    Exemplo03();
+                }
+                else if (opcaoDesejada != 8001)
+                {
+                    Console.WriteLine("Opção inválida");
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+
+
+            //Exemplo01();
+            //Exemplo02();
+            Exemplo03();
         }
 
         public void Exemplo01()
         {
-            Console.WriteLine("OIEEEEEEEEE");
+            int indice = 0;
+            while(indice < 10)
+            {
+                Console.WriteLine("Oie");
+                indice = indice + 1;
+            }
+
+            Console.WriteLine("Terminou o exemplo de While");
+        }
+
+        /// <summary>
+        /// Solicitar nome e sobrenome para 3 usuários.
+        /// </summary>
+
+        public void Exemplo02()
+        {
+            int indice = 0;
+            while(indice < 3)
+            {
+                // Inputs - entradas
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine().Trim();
+                Console.WriteLine("Sobrenome: ");
+                string sobrenome = Console.ReadLine().Trim();
+
+                // Processamento
+                string nomeCompleto = nome + " " + sobrenome;
+
+                // Output
+                Console.WriteLine("Nome completo: " + nomeCompleto);
+
+                indice = indice + 1;
+            }
+        }
+
+        /// <summary>
+        /// Exemplo pedir para o usuário a quantidade de notas
+        /// que deseja cadastrar, solicitando-as.
+        /// </summary>
+        public void Exemplo03()
+        {
+            Console.Write("Digite a quantidade de notas que deseja registrar: ");
+            int quantidadeDesejada = Convert.ToInt32 (Console.ReadLine());
+
+            int indice = 0;
+            // while(indice <= quantidadeDesejada - 1) outra forma de escrever o comando.
+            double somaNotas = 0, maiorNota = 0, menorNota = 8001.00;
+            while(indice < quantidadeDesejada)
+            {
+                Console.Write("Nota: ");
+                double nota = Convert.ToDouble(Console.ReadLine());
+
+                somaNotas = somaNotas + nota;
+
+                if(nota > maiorNota)
+                {
+                    maiorNota = nota;
+                }
+
+                if(nota < menorNota){
+                    menorNota = nota;
+                }
+
+                // Incrementar em 1
+                indice = indice + 1;
+            }
+
+            double media = somaNotas / quantidadeDesejada;
+            // \n é utilizado para quebrar a linha (para não precisar ficar digitando Console.WriteLine várias vezes).
+            Console.WriteLine(
+                "Média: " + media +
+                "\nMaior nota: " + maiorNota +
+                "\n Menor nota: " + menorNota);
         }
     }
 }
+
